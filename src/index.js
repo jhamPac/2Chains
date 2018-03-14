2a929e2fe16d634bbf59e4b468ff58c3845c6aba
Cloud <cloud@kevia.me>
2018-03-13 19:40:23 -0700

    added source dir; using dynamic timestamp

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ let p2p_port = process.env.P2P_PORT || 6001;
 let initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 let sockets = [];
+
 let MessageType = {
     QUERY_LATEST: 0,
     QUERY_ALL: 1,
@@ -16,10 +17,12 @@ let MessageType = {
 };
 
 const getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    return new Block(0, '0', Date.now(), 'genesis block', CryptoJS.SHA256('3V3'));
 };
 
 let blockchain = [getGenesisBlock()];
+
+console.log(blockchain)
 
 const initHttpServer = () => {
     let app = express();
